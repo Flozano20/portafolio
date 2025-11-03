27d86788e80ef1984a483e82baa3f7bc639ae1ca
Flozano <emboatic@gmail.com>
2025-11-02 19:50:39 -0600

    portafolio

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,25 @@
 import { defineConfig } from "vite";
+import path, { resolve } from 'node:path';
+import * as glob from 'glob';
+
+const obtenerEntradas = () => {
+  return Object.fromEntries(
+    glob.sync('./**/*.html', {
+      ignore: ['./dist/**', './node_modules/**']
+    }).map(file => [
+      file.slice(0, file.length - path.extname(file).length),
+      resolve(__dirname, file)
+    ])
+  );
+};
 
 export default defineConfig({
   appType: 'mpa',
-  base: '/portafolio/',
+  base: process.env.DEPLOY_BASE_URL,
   build: {
     rollupOptions: {
-      input: {
-        main: './index.html'
-      }
+      input: obtenerEntradas()
     },
-    minify: true,
-    outDir: 'dist'
-  }
+    minify: true
+  },
 });
